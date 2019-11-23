5aecbb57aa1d8c24487fc4bc42635718499f485e
Román Pozzo <rhpozzo@gmail.com>
2019-11-23 00:19:15 +0000

    added logic

--- src/js/component/trafficLight.js
+++ src/js/component/trafficLight.js
@@ -1,13 +1,38 @@
 import React from "react";
+import { useState } from "react";
 
 export const TrafficLight = () => {
+	const [light, setLight] = useState({ clickedLight: "" });
+
+	let redSelected = "";
+	if (light.clickedLight == "red") {
+		redSelected = "selected";
+	}
+	let yellowSelected = "";
+	if (light.clickedLight == "yellow") {
+		yellowSelected = "selected";
+	}
+	let greenSelected = "";
+	if (light.clickedLight == "green") {
+		greenSelected = "selected";
+	}
+
 	return (
 		<div className="main-container">
 			<div className="palito" />
 			<div className="lightContainer">
-				<div className="red light" />
-				<div className="yellow light" />
-				<div className="green light" />
+				<div
+					className={"light red " + redSelected}
+					onClick={() => setLight({ clickedLight: "red" })}
+				/>
+				<div
+					className={"light yellow " + yellowSelected}
+					onClick={() => setLight({ clickedLight: "yellow" })}
+				/>
+				<div
+					className={"light green " + greenSelected}
+					onClick={() => setLight({ clickedLight: "green" })}
+				/>
 			</div>
 		</div>
 	);
